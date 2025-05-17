836d84f6c0c38baa8a7677b84fa3138685eca23c
Shivansh <shivanshawasthi33@gmail.com>
2025-05-18 03:02:18 +0530

    hide warning

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,6 +27,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black text-white`}
+        suppressHydrationWarning={true}
       >
         <ClientScrollProgressBar />
         <UserDataSynchronizer />
