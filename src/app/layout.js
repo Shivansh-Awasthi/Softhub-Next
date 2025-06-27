659205322127511a96e335607d0ce9666b37d99f
Shivansh <shivanshawasthi33@gmail.com>
2025-06-28 00:00:26 +0530

    commented the restrictions for testing purpose

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,8 +33,8 @@ export default function RootLayout({ children }) {
       >
         <SnowEffect count={70} startAfterSidebar={true} />
         <ClientScrollProgressBar />
-        <UserDataSynchronizer />
-        <SecurityRestrictions />
+        {/* <UserDataSynchronizer /> */}
+        {/* <SecurityRestrictions /> */}
         <div className="flex min-h-screen">
           <SidebarWrapper />
           <div className="flex-1 w-full transition-all duration-300">
